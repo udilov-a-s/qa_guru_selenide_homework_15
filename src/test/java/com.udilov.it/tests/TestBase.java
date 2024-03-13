9cc56da4f6c28979b4f0717a6775d95b5313d26e
udilov-a-s <67996625+udilov-a-s@users.noreply.github.com>
2024-03-13 16:09:21 +0600

    Update TestBase.java

--- src/test/java/com.udilov.it/tests/TestBase.java
+++ src/test/java/com.udilov.it/tests/TestBase.java
@@ -24,7 +24,8 @@ public class TestBase {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browser_version", "122");
         Configuration.timeout = 6000;
-        Configuration.remote = System.getProperty("browser_remote_url");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //Configuration.remote = System.getProperty("browser_remote_url");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
